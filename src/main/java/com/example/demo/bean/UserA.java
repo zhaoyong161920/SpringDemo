5dcd47b5489c843a5d58851dbb33cb454fd13846
赵勇 <chongsi.zy@alibaba-inc.com>
2021-07-12 15:47:59 +0800

    添加复制方法测试

---
+++ src/main/java/com/example/demo/bean/UserA.java
@@ -0,0 +1,14 @@
+package com.example.demo.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+public class UserA {
+    int id;
+    String name;
+    String interest;
+}
---
+++ src/main/java/com/example/demo/bean/UserB.java
@@ -0,0 +1,13 @@
+package com.example.demo.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+public class UserB {
+    String name;
+    String score;
+}
