e753fc5f08ce586d9f4b574e53931c196d89c83a
赵勇 <chongsi.zy@alibaba-inc.com>
2021-07-12 15:50:41 +0800

    添加复制方法测试2

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.bean.UserA;
+import com.example.demo.bean.UserB;
 import com.example.demo.bo.Client;
 import com.example.demo.dao.ClientMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.connection.RedisConnection;
@@ -28,6 +31,18 @@ class DemoApplicationTests {
     @Test
     void contextLoads(){
         log.info("数据源：{}",dataSource.getClass());
+    }
+
+    @Test
+    void testCopy(){
+        UserB userB = new UserB("lisi", "123");
+        UserA userA = new UserA(1,"zhangsan","basketball");
+        log.info(userA.toString());
+        log.info(userB.toString());
+        log.info("----------");
+        BeanUtils.copyProperties(userB, userA); // 复制共有属性
+        log.info(userA.toString());
+        log.info(userB.toString());
     }
 
 }
